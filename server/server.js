7fc30b87786d00b419937b50b09d9918772bb540
Per Elofsson <elofper@gmail.com>
2025-03-07 16:36:07 +0100

    Databas plus två api-endpoints

--- server/server.js
+++ server/server.js
@@ -1,7 +1,38 @@
-const express = require ("express");
-const app = express();
+import express from "express";
+import Database from 'better-sqlite3'
 
+const db = new Database('./db/freakyfashion.db', {
+    verbose: console.log
+});
+
+// const express = require ("express"); //TODO: behövs den här raden?
+const app = express();
 const port = 8000;
+
+//för grid och sök
+app.get("/api/products", (req, res) => {
+    // Hämta alla rader från students-tabellen
+    const products = db.prepare('SELECT * FROM freakyfashion_stock').all();
+  
+    res.json(products);
+  
+  });
+
+//för productDetails
+  app.get("/api/products/:id", (req, res) => {
+
+    const productId = req.params.id;
+
+    const product = db.prepare('SELECT * FROM freakyfashion_stock WHERE id = ?').get(productId);
+
+    if (!product) {
+        res.status(404).send();
+        return;
+    }
+    res.json(product);
+  });
+
+  
 
 app.listen(port, () => {
     console.log(`Servern startad på port ${port}`)
