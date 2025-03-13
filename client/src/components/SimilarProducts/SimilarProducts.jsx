35766cc9fe27ff113e6716839e02ff9438592350
Per Elofsson <elofper@gmail.com>
2025-03-13 14:07:44 +0100

    Slide med produkter från databasen

--- client/src/components/SimilarProducts/SimilarProducts.jsx
+++ client/src/components/SimilarProducts/SimilarProducts.jsx
@@ -1,34 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./SimilarProducts.module.css"; 
 
 export default function SimilarProducts() {
     const [slideIndex, setSlideIndex] = useState(0);
 
+  const [products, setProducts] = useState([]);
 
-  // TODO Simulerade produkter (kan ersättas med data från API)
-  const products = Array(8).fill({
-    name: "Svart T-shirt",
-    price: "199 SEK",
-    brand: "Levis",
-    image: "https://placehold.co/600x700",
-    link: "/product_details",
-  });
-
+  useEffect(() => {
+    fetch("http://localhost:8000/api/products") // API-endpoint för produkter
+      .then((response) => response.json())
+      .then((data) => setProducts(data.slice(0, 8))) // Begränsar till 8 produkter
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
   const totalSlides = products.length;
-
+  
   // Hanterar slideförflyttning
   const changeSlide = (n) => {
-    let newIndex = slideIndex + n;
-
-    if (newIndex > totalSlides - 3) {
-        newIndex = 0;
-    } else if (newIndex < 0) {
-        newIndex = totalSlides - 3;
-    }
-    setSlideIndex(newIndex);
-  };
-
+      let newIndex = slideIndex + n;
+      
+      if (newIndex > totalSlides - 3) {
+          newIndex = 0;
+        } else if (newIndex < 0) {
+            newIndex = totalSlides - 3;
+        }
+        setSlideIndex(newIndex);
+    };
+    
     return(
         <section className={styles.liknandeProdukter}>
       <h2>Liknande produkter</h2>
@@ -39,13 +37,13 @@ export default function SimilarProducts() {
         >
           {products.map((product, index) => (
             <div key={index} className={styles.slide}>
-              <Link to={product.link} className={styles.link}>
-                <img src={product.image} alt={product.name} />
+              <Link to={`/products/${product.product_slug}`} className={styles.link}>
+                <img src={product.product_image} alt={product.product_name} />
                 <div className={styles.liknandeProdukterInfo}>
-                  <h2>{product.name}</h2>
-                  <h2>{product.price}</h2>
+                  <h2>{product.product_name}</h2>
+                  <h2>{product.product_price}</h2>
                 </div>
-                <div className={styles.brandname}>{product.brand}</div>
+                <div className={styles.brandname}>{product.product_brand}</div>
               </Link>
             </div>
           ))}
