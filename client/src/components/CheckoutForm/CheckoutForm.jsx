1cf7913f7e5a8553d630fad78fb034e24b9332bb
Per Elofsson <elofper@gmail.com>
2025-03-23 22:52:52 +0100

    Semantik

--- client/src/components/CheckoutForm/CheckoutForm.jsx
+++ client/src/components/CheckoutForm/CheckoutForm.jsx
@@ -8,37 +8,59 @@ export default function CheckoutForm() {
         <div className={styles.nameContainer}>
           <div>
             <label htmlFor="firstName">Förnamn</label>
-            <input type="text" name="firstName" id="" />
+            <input type="text" name="firstName" id="firstName" required />
           </div>
           <div>
             <label htmlFor="lastName">Efternamn</label>
-            <input type="text" name="firstName" id="" />
+            <input type="text" name="lastName" id="lastName" required />
           </div>
           <div>
             <label htmlFor="email">E-post</label>
-            <input type="text" name="firstName" id="" />
+            <input type="email" name="email" id="email" required />
           </div>
         </div>
         <fieldset className={styles.fieldsetCheckout}>
           <legend>Adress</legend>
           <div>
             <label htmlFor="street">Gata</label>
-            <input className={styles.streetInput} type="text" name="street" id=""/>
+            <input
+              className={styles.streetInput}
+              type="text"
+              name="street"
+              id="street"
+              required
+            />
           </div>
           <div>
             <label htmlFor="postalcode">Postnummer</label>
-            <input className={styles.postalInput} type="text" name="firstName" id="" />
+            <input
+              className={styles.postalInput}
+              type="text"
+              name="postalcode"
+              id="postalcode"
+              pattern="[0-9]{5}" 
+              title="Postnummer ska vara 5 siffror"
+              required
+            />
           </div>
           <div>
             <label htmlFor="city">Stad</label>
-            <input className={styles.cityInput} type="text" name="firstName" id="" />
+            <input
+              className={styles.cityInput}
+              type="text"
+              name="city"
+              id="city"
+              required
+            />
           </div>
         </fieldset>
-        <label >
-          <input className={styles.newsletterCheckbox} type="checkbox" />
+        <label className={styles.newsLetterLabel}>
+          <input type="checkbox"
+            name="newsletter"
+            id="newsletter" />
           Jag vill ta emot nyhetsbrev
         </label>
-        <button>Köp</button>
+        <button type="button" aria-label="Genomför köp">Köp</button>
       </form>
     </>
   );
